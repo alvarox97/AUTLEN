df5c84771a7f5ba9a715b45684777db61071e0d2
Tajul2810 <bytajul2810@gmail.com>
2018-10-04 21:24:07 +0200

    palabra.c

--- palabra.c
+++ palabra.c
@@ -2,19 +2,111 @@
 #include <string.h>
 #include <stdint.h>
 #include "palabra.h"
-#define TAMHASH 157286
 
 /**
  * Implementacion de hash con una lista enlazada para controlar
  * colisiones
  */
-typedef struct _hash_node{
-    void* value;
-    char* key;
-    struct _hash_node* next;
-}hash_node;
+typedef struct _palabra{
+  char** simbolos;
+  int size;
+}palabra;
 
-typedef struct _hash{
-    hash_node **data;
-    void (*free_value)(void *);
-}hash;
+palabra* crear_palabra(){
+
+  palabra* palabra = NULL;
+
+  if(!simbolos){
+    return NULL;
+  }
+
+  /*Asignamos memoria para la palabra*/
+  palabra = (palabra *) malloc(sizeof(palabra));
+
+  if(!palabra){
+    return NULL;
+  }
+
+  /*Asignamos memoria para el puntero de simbolos*/
+  palabra->simbolos = (char **) malloc(sizeof(char *));
+
+  if(!palabra->simbolos){
+    free(palabra);
+    return NULL;
+  }
+
+  /*Asignamos el tamaño de la palabra*/
+  palabra->size = 0;
+
+  /*Devolvemos la palabra*/
+  return palabra;
+}
+
+void eliminar_palabra(palabra* palabra){
+  int i = 0:
+  if(!palabra){
+    return;
+  }
+
+  if(!palabra->simbolos){
+    free(palabra):
+    return;
+  }
+
+  /*En caso de tener simbolos los liberamos tambien*/
+  for(i=0; i<palabra->size; i++){
+    if(palabra->simbolos[i]){
+      free(palabra->simbolos[i]);
+    }
+  }
+  free(palabra->simbolos);
+  free(palabra);
+}
+
+int palabra_get_size(palabra* palabra){
+  if(!palabra){
+    return -1;
+  }
+
+  return palabra->size;
+}
+
+char** palabra_get_simbolos(palabra* palabra){
+  if(!palabra){
+    return NULL;
+  }
+
+  return palabra->simbolos;
+}
+
+char* palabra_insertar_simbolo(palabra* palabra, char* simbolo){
+  if(!palabra || !simbolo){
+    return NULL;
+  }
+
+  /*Comprobamos si existe suficiente memoria en el array*/
+  if(palabra->size >= sizeof(*palabra->simbolos)/sizeof(char*)){
+    /*Si no la hay hacemos realloc y añadimos memoria para un simbolo*/
+		palabra->simbolos = (char**) realloc(palabra->simbolos, sizeof(*palabra->simbolos) + sizeof(char*));
+  }
+
+  /*Comprobamos que el puntero no ha explosionado como una bomba H*/
+  if(!palabra->simbolos){
+    return NULL;
+  }
+
+  /*Reservamos memoria para el nuevo simbolo*/
+  palabra->simbolos[palabra->size] = (char*) malloc(sizeof(char)*(strlen(simbolo) + 1));
+
+  if(!palabra->simbolos[palabra->size]){
+    return NULL;
+  }
+
+  /*Añadimos el simbolo a la palabra*/
+  if(!strcpy(palabra->simbolos[palabra->size], simbolo)){
+    return NULL;
+  }
+
+  /*Si todo correcto devolvemos la palabra*/
+  return palabra;
+}
--- palabra.h
+++ palabra.h
@@ -3,5 +3,14 @@
 
 typedef struct palabra;
 
+palabra* crear_palabra();
+
+void eliminar_palabra(palabra* palabra);
+
+int palabra_get_size(palabra* palabra);
+
+char** palabra_get_simbolos(palabra* palabra);
+
+char* palabra_insertar_simbolo(palabra* palabra, char* simbolo);
 
 #endif
